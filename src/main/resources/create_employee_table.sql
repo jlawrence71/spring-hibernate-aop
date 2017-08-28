7894642b653812ba4ecadc2f77fb74702193c859
James <jlawrence71@yahoo.com>
2017-08-28 08:34:05 -0500

    first commit

---
+++ src/main/resources/create_employee_table.sql
@@ -0,0 +1,6 @@
+create table employee(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(256) NOT NULL,
+  salary float(22),
+  PRIMARY KEY(id)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
